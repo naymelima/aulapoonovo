307e5284da2f0549c53344910db4d551605c5415
naymelima <nayme.lima07@gmail.com>
2026-09-10 10:29:47 -0300

    teste

--- Jhonatan perfeição.py
+++ Jhonatan perfeição.py
@@ -1,1 +1,25 @@
-nome = estefany
+class funcionario:
+    def __init__(self, nome, salário):
+        self.nome = nome
+        self.salario = salário
+        calcular_salario()
+
+class gerente:
+    def __init__(self, nome, salário):
+        self.nome = nome 
+        self.salario = salário 
+        calcular_salario()
+
+class estagiario:
+    def __init__(self, nome, salário):
+        self.nome = nome 
+        self.salario = salário
+        calcular_salario()
+
+def mostrar_salario(funcionario):
+    print(f'O salário do funcinário {funcionario.nome} é {funcionario.salario}.')
+
+
+
+
+
---
+++ aula2poo.py
@@ -0,0 +1,33 @@
+class Livro:
+    def __init__(self, titulo, autor):
+        self.titulo = titulo
+        self.autor = autor
+
+        def ler(self):
+            return "Estou lendo um livro."
+
+        class Romance(Livro):
+            def ler(self):
+                return "Estou lendo um romance."
+
+        class Terror(livro):
+            def ler(self):
+                return "Estou lendo um livro de terror."
+
+            class ficcao(Livro):
+                def ler(self):
+                    return "Estou lendo um livro de ficção."
+
+
+        livro1 = Romance("Orgulho e Preconceito", "Jane Austen")
+        livro2 = Terror("O Iluminado", "Stephen King")
+        livro3 = ficcao("Duna", "Frank Herbert")
+
+
+        livros = [livro1, livro2, livro3]
+
+        for livro in livros:
+            print("titulo:", livro.titulo)
+            print("autor:", livro.autor)
+            print(livro.ler())
+            print()
